d5becb917c5f57cb286fb6f15c1b5c281f254be5
Harvey <me@ngvuong.com>
2022-01-20 23:21:33 +0100

    add choice to run unittest

--- snorlax/main.py
+++ snorlax/main.py
@@ -10,15 +10,17 @@ if __name__ == '__main__':
 
     flag = True
     while flag:
-        print("Welcome to simple crawler! (Y) Continue, (Q) Quit.")
+        print("Welcome to simple crawler! (Y) Continue, (T) Run unittest, (Q) Quit.")
         choice = input("Choice: ")
         if choice == 'Q':
             exit()
+        if choice == 'T':
+            pass
         if choice == 'Y':
             url = input("Enter an url to crawl events: ")
             events = Crawler(url).events
 
-            print("(Y) Write to db, (Q) Quit, (Otherwise) Back to welcome screen.")
+            print("(Y) Write to db, (Q) Quit, (Others) Back to welcome screen.")
             choice = input("Choice: ")
             if choice == 'Y':
                 Writer(events=events).to_db()
