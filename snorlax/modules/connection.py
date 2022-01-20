e84399ff322c71230b8cf66c318803314ac23c23
Harvey <me@ngvuong.com>
2022-01-20 13:42:23 +0100

    config postgresql

---
+++ snorlax/modules/connection.py
@@ -0,0 +1,28 @@
+from typing import Tuple
+
+import psycopg2
+from .config import config
+
+
+class Connection:
+    def __init__(self):
+        params = config()
+        self.cur = None
+        self.conn = psycopg2.connect(**params)
+        self.cur = self.conn.cursor()
+
+    def exec_select(self, query: str, items: Tuple = None):
+        self.cur.execute(query, items)
+        query_results = self.cur.fetchall()
+        return query_results
+
+    def exec_insert(self, query: str, items: Tuple):
+        self.cur.execute(query, items)
+        item_id = self.cur.fetchone()[0]
+        self.conn.commit()
+        return item_id
+
+    def close(self):
+        self.conn.close()
+
+
