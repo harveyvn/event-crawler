d93b0ff9b8aa4df0c09effc95391dcb1f4f7e73e
Harvey <me@ngvuong.com>
2022-01-20 22:22:48 +0100

    add spinner to writer

--- snorlax/modules/writer.py
+++ snorlax/modules/writer.py
@@ -1,5 +1,6 @@
 import psycopg2
 from typing import List
+from yaspin import yaspin
 from .constant import CONST
 from .connection import Connection
 from .common import timestamp
@@ -13,34 +14,36 @@ class Writer:
     def to_db(self):
         events = self.events
         conn = None
-        try:
-            print("Connecting to the PostgreSQl database...")
-            conn = Connection()
-            print(f'PostgreSQl database version: {conn.exec_select("SELECT version()")}')
+        with yaspin(text="Writing an event to db!", color="yellow") as spinner:
+            try:
+                print("\nConnecting to the PostgreSQl database...")
+                conn = Connection()
+                print(f'PostgreSQl database version: {conn.exec_select("SELECT version()")}')
+                for idx, event in enumerate(events):
+                    event_id = conn.process(Event(title=event[CONST.TITLE]))
+                    date = timestamp(event[CONST.DATE], event[CONST.HOUR], event[CONST.MINS])
 
-            for event in events:
-                event_id = conn.process(Event(title=event[CONST.TITLE]))
-                date = timestamp(event[CONST.DATE], event[CONST.HOUR], event[CONST.MINS])
+                    for location in event[CONST.LOCATIONS]:
+                        location_id = conn.process(Location(name=location))
+                        conn.process(EventLocation(event_id, location_id, date))
 
-                for location in event[CONST.LOCATIONS]:
-                    location_id = conn.process(Location(name=location))
-                    conn.process(EventLocation(event_id, location_id, date))
+                    for cover in event[CONST.COVER]:
+                        cover_id = conn.process(Cover(url=cover))
+                        conn.process(EventCover(event_id, cover_id))
 
-                for cover in event[CONST.COVER]:
-                    cover_id = conn.process(Cover(url=cover))
-                    conn.process(EventCover(event_id, cover_id))
+                    for artist in event[CONST.PROGRAM][CONST.ARTISTS]:
+                        artist_id = conn.process(Artist(name=artist))
+                        conn.process(EventArtist(event_id, artist_id))
 
-                for artist in event[CONST.PROGRAM][CONST.ARTISTS]:
-                    artist_id = conn.process(Artist(name=artist))
-                    conn.process(EventArtist(event_id, artist_id))
+                    for song in event[CONST.PROGRAM][CONST.SONGS]:
+                        song_id = conn.process(Song(title=song))
+                        conn.process(EventSong(event_id, song_id))
 
-                for song in event[CONST.PROGRAM][CONST.SONGS]:
-                    song_id = conn.process(Song(title=song))
-                    conn.process(EventSong(event_id, song_id))
+                    spinner.ok(f'✅ Done event {idx}!')
 
-        except(Exception, psycopg2.DatabaseError) as e:
-            print("⛔️ Exception:", e)
-        finally:
-            if conn is not None:
-                conn.close()
-                print("Database connection closed.")
+            except(Exception, psycopg2.DatabaseError) as e:
+                spinner.fail(f'💥 Exception! {e}')
+            finally:
+                if conn is not None:
+                    conn.close()
+                    print("Database connection closed.")
