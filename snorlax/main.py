e96bcb7dffb9cebc36e91986b6abfe2a81ac6c6e
Harvey <me@ngvuong.com>
2022-01-21 01:27:20 +0100

    dockerize script

--- snorlax/main.py
+++ snorlax/main.py
@@ -8,25 +8,21 @@ if __name__ == '__main__':
     urls = ["https://www.lucernefestival.ch/en/program/summer-festival-22",
             "https://www.lucernefestival.ch/en/program/mendelssohn-festival-22"]
 
+    flag = True
+    while flag:
+        print("Welcome to simple crawler! (Y) Continue, (T) Run unittest, (Q) Quit.")
+        choice = input("Choice: ")
+        if choice == 'Q':
+            exit()
+        if choice == 'T':
+            pass
+        if choice == 'Y':
+            url = input("Enter an url to crawl events: ")
+            events = Crawler(url).events
 
-    events = Crawler(urls[0]).events
-    Writer(events=events).to_db()
-
-    # flag = True
-    # while flag:
-    #     print("Welcome to simple crawler! (Y) Continue, (T) Run unittest, (Q) Quit.")
-    #     choice = input("Choice: ")
-    #     if choice == 'Q':
-    #         exit()
-    #     if choice == 'T':
-    #         pass
-    #     if choice == 'Y':
-    #         url = input("Enter an url to crawl events: ")
-    #         events = Crawler(url).events
-
-    #         print("(Y) Write to db, (Q) Quit, (Others) Back to welcome screen.")
-    #         choice = input("Choice: ")
-    #         if choice == 'Y':
-    #             Writer(events=events).to_db()
-    #         elif choice == 'Q':
-    #             exit()
+            print("(Y) Write to db, (Q) Quit, (Others) Back to welcome screen.")
+            choice = input("Choice: ")
+            if choice == 'Y':
+                Writer(events=events).to_db()
+            elif choice == 'Q':
+                exit()
